37cb447892db6c2501d1da5bdb7fcad53c60ea53
iCompra <iCompra.contacto@gmail.com>
2025-07-12 20:19:31 -0400

    Updated trace

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "b2f203b9204f6629"; }
+/******/ 		__webpack_require__.h = function() { return "60c258a951c3c6bc"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
